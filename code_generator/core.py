2d093e702a5d23df372e9c068d17035ed24f6da1
hamidreza <hamidreza004@gmail.com>
2020-02-06 15:11:32 +0330

    Add read/write

--- code_generator/core.py
+++ code_generator/core.py
@@ -6,12 +6,21 @@ code = []
 
 
 def initiate():
-    code.append("""@.i32 = private unnamed_addr constant[4 x i8] c"%d\0A\00", align 1""")
-    code.append("""@.i64 = private unnamed_addr constant[4 x i8] c"%d\0A\00", align 1""")
-    code.append("""@.i8 = private unnamed_addr constant[4 x i8] c"%d\0A\00", align 1""")
-    code.append("""@.i16 = private unnamed_addr constant[4 x i8] c"%d\0A\00", align 1""")
-
+    code.append("""@.wi1 = private unnamed_addr constant[5 x i8] c"%d \0A\00", align 1""")
+    code.append("""@.wi8 = private unnamed_addr constant[5 x i8] c"%c \0A\00", align 1""")
+    code.append("""@.wi32 = private unnamed_addr constant[5 x i8] c"%d \0A\00", align 1""")
+    code.append("""@.wi64 = private unnamed_addr constant[5 x i8] c"%ld\0A\00", align 1""")
+    code.append("""@.wfloat = private unnamed_addr constant[5 x i8] c"%f \0A\00", align 1""")
+    code.append("""@.wstring = private unnamed_addr constant[5 x i8] c"%s \0A\00", align 1""")
+    code.append("""@.ri1 = private unnamed_addr constant[3 x i8] c"%d\00", align 1""")
+    code.append("""@.ri8 = private unnamed_addr constant[3 x i8] c"%c\00", align 1""")
+    code.append("""@.ri32 = private unnamed_addr constant[3 x i8] c"%d\00", align 1""")
+    code.append("""@.ri64 = private unnamed_addr constant[4 x i8] c"%ld\00", align 1""")
+    code.append("""@.rfloat = private unnamed_addr constant[3 x i8] c"%f\00", align 1""")
+    code.append("""@.rstring = private unnamed_addr constant[3 x i8] c"%s\00", align 1""")
+    code.append("""declare i32 @printf(i8*, ...)""")
+    code.append("""declare i32 @__isoc99_scanf(i8*, ...)""")
 
 
 def generate(op, token):
-    result = getattr(funcs, op)(code, sym_table, token)
+    getattr(funcs, op)(code, sym_table, token)
--- code_generator/funcs.py
+++ code_generator/funcs.py
@@ -3,11 +3,11 @@ from code_generator.helper import variable_map, variable_size, type_cast
 diff_count = 0
 
 
-def push(code, token, sem_stack):
+def push(_, token, sem_stack):
     sem_stack.append({'name': token})
 
 
-def pop(code, token, sem_stack):
+def pop(_, __, sem_stack):
     sem_stack.pop()
 
 
@@ -19,29 +19,55 @@ def def_var(code, token, sem_stack):
     sem_stack.append(var)
 
 
-def cast(code, type, sem_stack):
-    var = sem_stack.pop()
+def load_var(code, var):
+    global diff_count
+    code.append(f"%.tmp{diff_count} = load {var['type']}, {var['type']}* %{var['name']}, align {var['align']}")
+
+
+def store_var(code, var):
+    global diff_count
+    code.append(f"store {var['type']} %.tmp{diff_count}, {var['type']}* %{var['name']}, align {var['align']}")
+
+
+def cast(code, var, type):
+    global diff_count
     if var['type'] == type:
         return
-    code.append(f"%tmpp{diff_count} = {type_cast(var['type'], type)} {var['type']} {var['name']} to {type}")
+    code.append(f"%.tmp{diff_count} = {type_cast(var['type'], type)} {var['type']} {var['name']} to {type}")
 
 
-def assign_var(code, token, sem_stack):
+def assign_var(code, _, sem_stack):
     global diff_count
     var_a = sem_stack.pop()
     var_b = sem_stack.pop()
 
-    code.append(f"%tmpp{diff_count} = load {var_a['type']}, {var_a['type']}* %{var_a['name']}, align {var_a['align']}")
+    load_var(code, var_a)
 
     if var_b['type'] != var_a['type']:
-        sem_stack.append({'name': f'tmpp{diff_count}', 'type': var_a['type'], 'align': var_a['align']})
+        tmp_var = {'name': f'.tmp{diff_count}', 'type': var_a['type'], 'align': var_a['align']}
         diff_count += 1
-        cast(code, var_b['type'], sem_stack)
+        cast(code, tmp_var, var_b['type'])
 
-    code.append(f"store {var_b['type']} %tmpp{diff_count}, {var_b['type']}* %{var_b['name']}, align {var_b['align']}")
+    store_var(code, var_b)
     diff_count += 1
 
-def write(code, token, sem_stack):
+
+def write(code, _, sem_stack):
+    global diff_count
     var = sem_stack.pop()
+    load_var(code, var)
+    code.append(
+        f""" call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([5 x i8], [5 x i8]* @.w{var['type']}, i32 0, i32 
+        0), {var['type']} %.tmp{diff_count}) """)
+    diff_count += 1
 
 
+def read(code, _, sem_stack):
+    global diff_count
+    var = sem_stack.pop()
+    if var['type'] == 'i64':
+        code.append(
+            f"""call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.r{var['type']}, i32 0, i32 0), {var['type']}* %{var['name']})""")
+    else:
+        code.append(
+            f"""call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.r{var['type']}, i32 0, i32 0), {var['type']}* %{var['name']})""")
